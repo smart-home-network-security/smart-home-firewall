053e19580eac4f319fce3d622ac43337ac7e4cfc
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2024-07-09 16:50:44 +0200

    Created submodule protocols translators

--- src/translator/protocols/tcp.py
+++
@@ -1,9 +0,0 @@
-from protocols.Transport import Transport
-
-class tcp(Transport):
-    
-    # Class variables
-    protocol_name = "tcp"  # Protocol name
-
-    # Supported keys in YAML profile
-    supported_keys = Transport.supported_keys + ["initiated-by"]
--- src/translator/protocols/udp.py
+++
@@ -1,6 +0,0 @@
-from protocols.Transport import Transport
-
-class udp(Transport):
-    
-    # Class variables
-    protocol_name = "udp"  # Protocol name
