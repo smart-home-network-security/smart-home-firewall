e5bf4b5b6c61d94cddb5ee66736be4fee77c2163
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2024-07-18 11:18:50 +0200

    CI: execut binaries

---
+++ .ci_scripts/full-test/run_exec.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Constants
+TIMEOUT=5  # seconds
+BIN_DIR="$GITHUB_WORKSPACE/bin"
+
+# Ensure globbing expands to an empty list if no matches are found
+shopt -s nullglob
+
+# Execute all NFQueue executables
+for EXEC in "$BIN_DIR"/*
+do
+    if [ -f "$EXEC" ]
+    then
+        ARG=""
+        if [[ "$EXEC" == *"/nflog" ]]
+        then
+            ARG="100"
+        fi
+        sudo $EXEC $ARG & sleep $TIMEOUT
+        sudo kill $!
+    fi
+done
